ee71c93d4e0f33f44d945923eef0fe8a2536bd23
cbrzn <cesarbrazon10@gmail.com>
2018-05-09 21:45:52 -0400

    detail

--- client/components/Messages.js
+++ client/components/Messages.js
@@ -1,19 +1,10 @@
 import React from "react";
 import Sender from "./Sender";
 import io from 'socket.io-client';
-import axios from 'axios'
 
 class Messages extends React.Component {
 
   componentDidMount() {
-    var url = '/value';
-    axios.get(url).then((result) => {
-      if (result.data.status != 400) {
-        socket.emit('add user', result.data.status);
-      } else {
-        socket.emit('add user', 'motherfucker');
-      }
-    })
     const { socket, newMessage } = this.props;
     socket.emit('login')
     socket.on('new message', (data) => {

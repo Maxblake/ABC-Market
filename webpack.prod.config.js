98628e9900658acee9cf114768b4df623fa4892d
cbrzn <cesarbrazon10@gmail.com>
2018-08-29 19:10:04 -0400

    adding production webpack (trying to)

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,11 +10,17 @@ module.exports = {
         publicPath: '/static/'
     },
     plugins: [
+        new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
-            compress: {
-                warnings: false
-            }
+          minimize: true,
+          compress: {
+            warnings: false
+          }
+        }),
+        new webpack.DefinePlugin({
+          'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+          }
         })
     ],
     module: {

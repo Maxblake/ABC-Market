e4454269477240f109b167c57df69818c77a1ee8
cbrzn <cesarbrazon10@gmail.com>
2018-08-29 21:27:03 -0400

    adding production webpack (trying to)

--- client/index.js
+++ client/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './src/index.css';
 import App from './src/App';
-import registerServiceWorker from './src/registerServiceWorker';
 import Auth from './src/Provider/Auth'
 
 
 ReactDOM.render(<Auth> <App /> </Auth>, document.getElementById('main'));
-registerServiceWorker();
